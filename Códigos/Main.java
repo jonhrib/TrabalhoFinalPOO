62339a85d9c14cb1cf818251e76a49aebf19823e
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-16 18:27:48 -0300

    Update Main.java

--- Códigos/Main.java
+++ Códigos/Main.java
@@ -1,1 +1,24 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Scanner s = new Scanner (System.in);
+		
+//		Cliente c = new Cliente ();
+//		
+//		c.setAgencia("0083");
+//		c.setIdade(15);
+//		c.setNome("Jorginho");
+//		
+//		System.out.println(c.getNome());
+//		System.out.println(c.getAgencia());
+//		System.out.println(c.getIdade());
+		
+		Gerente g = new Gerente ();
+		
+		g.criarconta();
+	}
+}
 
