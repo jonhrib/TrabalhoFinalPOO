bccb2f5ca9cef094b1089d3d1b84311fc313c0ef
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-16 10:38:21 -0300

    Update Cliente.java

--- Códigos/Cliente.java
+++ Códigos/Cliente.java
@@ -1,1 +1,44 @@
 
+public class Cliente extends Conta {
+	String numconta;
+	int idade;
+	String cpf;
+	String tipodeconta;
+	double renda;
+	String telefone;
+	double saldo;
+	double divida;
+
+	@Override
+	void acessar() {
+		
+	}
+	
+	void editarconta () {
+		
+	}
+	
+	void extrato () {
+		
+	}
+	
+	void consultardivida () {
+		
+	}
+	
+	double saque () {
+		
+		return 0;
+	}
+	
+	double deposito () {
+		
+		return 0;
+	}
+	
+	boolean pediremprestimo () {
+		
+		return false;
+	}
+}
+
