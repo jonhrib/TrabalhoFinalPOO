83f647a651c0914d9cb7bf3e4ca2625182bc04cc
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-16 10:38:48 -0300

    Update Gerente.java

--- Códigos/Gerente.java
+++ Códigos/Gerente.java
@@ -1,1 +1,24 @@
 
+public class Gerente extends Conta {
+	String ID;
+	
+	@Override
+	void acessar() {
+		
+	}
+	
+	void criarconta () {
+		
+	}
+	
+	void excluirconta () {
+		
+	}
+	
+	boolean avaliaremprestimo () {
+		
+		return false;
+	}
+
+}
+
