e2bd7d47932a48a3802e8a7e0cc3068d85ed30de
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-16 18:27:26 -0300

    Update Gerente.java

--- Códigos/Gerente.java
+++ Códigos/Gerente.java
@@ -1,13 +1,27 @@
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Gerente extends Conta {
+	
+	Gerente() throws ClassNotFoundException, SQLException {
+		super();
+		
+	}
+
 	String ID;
 	
 	@Override
-	void acessar() {
+	boolean acessar() {
 		
+		return false;
 	}
 	
-	void criarconta () {
+	void criarconta () throws SQLException {
+	
+//		String SQLInsert = "insert into cliente (nome,uf,agencia,numconta,idade,cpf,senha,tipodeconta,renda,saldo,divida,telefone) VALUES ('Jorge','PR','0001','003',18,'000.115.879-98','123456AB','Estudantil',2500,256.89,5000,'(43)xxxx-xxxx')";
+//		Statement stmt = con.createStatement();
+//		stmt.executeUpdate(SQLInsert);
+		
 		
 	}
 	
