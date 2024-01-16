a64552961a42c255a8aa92be9e771194576ba858
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-16 11:44:04 -0300

    Update Conta.java

--- Códigos/Conta.java
+++ Códigos/Conta.java
@@ -6,5 +6,36 @@ public abstract class Conta {
 	String nome;
 	
 	abstract void acessar ();
-}
 
+	public String getUF() {
+		return UF;
+	}
+
+	public void setUF(String uF) {
+		UF = uF;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
