2ef5b8791b62e7533ae5d31abf24662d425da380
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-19 18:54:16 -0300

    Update MenuCliente.java

--- Códigos/InterfaceGrafica/MenuCliente.java
+++ Códigos/InterfaceGrafica/MenuCliente.java
@@ -15,6 +15,8 @@ import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.sql.SQLException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MenuCliente extends JFrame {
 
@@ -59,7 +61,7 @@ public class MenuCliente extends JFrame {
 		textPane.setEditable(false);
 		textPane.setBackground(new Color(128, 128, 255));
 		textPane.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 14));
-		textPane.setText("UF: " + s[1] + "| Agência: " + agencia + " | Número da conta: " + conta + " | Tipo de conta: " + s[4] + " | Nome: " + s[0] + " | Idade: " + s[2] + " | CPF: " + s[3] + " | Telefone: " + s[5]);
+		textPane.setText("UF: " + s[1] + " | Agência: " + agencia + " | Número da conta: " + conta + " | Tipo de conta: " + s[4] + " | Nome: " + s[0] + " | Idade: " + s[2] + " | CPF: " + s[3] + " | Telefone: " + s[5]);
 		menuBar.add(textPane);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -77,40 +79,81 @@ public class MenuCliente extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Editar dados pessoais");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnNewButton.setBounds(224, 82, 245, 85);
 		btnNewButton.setForeground(new Color(0, 0, 160));
 		btnNewButton.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		contentPane.add(btnNewButton);
 		
 		JButton btnConsultarExtrato = new JButton("Pedir Empréstimo");
+		btnConsultarExtrato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnConsultarExtrato.setForeground(new Color(0, 0, 160));
 		btnConsultarExtrato.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		btnConsultarExtrato.setBounds(224, 213, 245, 85);
 		contentPane.add(btnConsultarExtrato);
 		
 		JButton btnSaque = new JButton("Saque");
+		btnSaque.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnSaque.setForeground(new Color(0, 0, 160));
 		btnSaque.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		btnSaque.setBounds(224, 345, 245, 85);
 		contentPane.add(btnSaque);
 		
 		JButton btnNewButton_2_1 = new JButton("Depósito");
+		btnNewButton_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnNewButton_2_1.setForeground(new Color(0, 0, 160));
 		btnNewButton_2_1.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		btnNewButton_2_1.setBounds(508, 345, 245, 85);
 		contentPane.add(btnNewButton_2_1);
 		
 		JButton btnNewButton_1_1 = new JButton("Consultar Dívida");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnNewButton_1_1.setForeground(new Color(0, 0, 160));
 		btnNewButton_1_1.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		btnNewButton_1_1.setBounds(508, 213, 245, 85);
 		contentPane.add(btnNewButton_1_1);
 		
 		JButton btnConsultarExtrato_1 = new JButton("Consultar Extrato");
+		btnConsultarExtrato_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnConsultarExtrato_1.setForeground(new Color(0, 0, 160));
 		btnConsultarExtrato_1.setFont(new Font("BancoDoBrasil Textos", Font.PLAIN, 16));
 		btnConsultarExtrato_1.setBounds(508, 82, 245, 85);
 		contentPane.add(btnConsultarExtrato_1);
+		
+		JButton btnNewButton_1 = new JButton("Voltar para a tela de login");
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Inicial i = new Inicial();
+				i.setVisible(true);
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(10, 484, 174, 21);
+		contentPane.add(btnNewButton_1);
 	}
 }
-
