f82f0d10d6f1f05343d775313d48e7bb9ae9a455
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-01-26 19:52:41 -0300

    Add files via upload

---
+++ Códigos/Classes/Gerente.java
@@ -0,0 +1,244 @@
+package Classes;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Gerente extends Conta {
+	
+	public Gerente() throws ClassNotFoundException, SQLException {
+		super();
+		
+	}
+
+	String ID;
+	ArrayList<String []> pedidosdeemprestimo = new ArrayList<String []>();
+	
+	@Override
+	public boolean acessar(int cod, String s, String s1, String s2) throws SQLException {
+		Statement stmt = con.createStatement();
+		boolean result = false;
+		
+		if (cod == 1) { //Agencia
+			//"select nome,uf,agencia,numconta,idade,cpf,senha,tipodeconta,renda,saldo,divida,telefone from cliente"
+			ResultSet dados = stmt.executeQuery("select agencia from gerente");
+			if (dados.isBeforeFirst()) {
+				
+				while (dados.next()) {
+					String agencia = dados.getString(1);
+					if (s.equals(agencia)) {
+						result = true;
+					}
+				}
+			}
+			
+			return result;
+		} 
+		else if (cod == 2) { //ID
+			ResultSet dados = stmt.executeQuery("select agencia,id from gerente");
+			if (dados.isBeforeFirst()) {
+				
+				while (dados.next()) {
+					String agencia = dados.getString(1);
+					String id = dados.getString(2);
+					if (s.equals(agencia) && s1.equals(id)) {
+						result = true;
+					}
+				}
+			}
+			
+			return result;
+		}
+		else if (cod == 3) { //Senha
+			ResultSet dados = stmt.executeQuery("select agencia,id,senha from gerente");
+			if (dados.isBeforeFirst()) {
+				
+				while (dados.next()) {
+					String agencia = dados.getString(1);
+					String id = dados.getString(2);
+					String senha = dados.getString(3);
+					
+					if (s.equals(agencia) && s1.equals(id) && s2.equals(senha)) {
+						result = true;
+					}
+				}
+			}
+			
+			return result;
+		}
+		else return false;
+	}
+	
+	public String[] encontrardados (String conta) throws SQLException {
+		Statement stmt = con.createStatement();
+		String nome, uf, agencia, numconta, idade, cpf, senha, tipodeconta, renda, saldo, divida, telefone;
+		
+		ResultSet dados = stmt.executeQuery("select numconta, nome, uf, agencia, idade, cpf, senha, tipodeconta, renda, saldo, divida, telefone from cliente");
+		if (dados.isBeforeFirst()) {
+			
+			while (dados.next()) {
+				String teste = dados.getString(1);
+				if (conta.equals(teste)) {
+					nome = dados.getString(2);
+					uf = dados.getString(3);
+					agencia = dados.getString(4);
+					idade = dados.getString(5);
+					cpf = dados.getString(6);
+					senha = dados.getString(7);
+					tipodeconta = dados.getString(8);
+					renda = dados.getString(9);
+					saldo = dados.getString(10);
+					divida = dados.getString(11);
+					telefone = dados.getString(12);
+					String[] s = {nome,uf,agencia, idade, cpf, senha, tipodeconta, renda, saldo, divida, telefone};
+					return s;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public String[] encontranome (String id) throws SQLException {
+		Statement stmt = con.createStatement();
+		String nome, uf, senha;
+		
+		ResultSet dados = stmt.executeQuery("select id,nome,uf,senha from gerente");
+		if (dados.isBeforeFirst()) {
+			
+			while (dados.next()) {
+				String teste = dados.getString(1);
+				if (id.equals(teste)) {
+					nome = dados.getString(2);
+					uf = dados.getString(3);
+					senha = dados.getString(4);
+					String[] s = {nome,uf,senha};
+					return s;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public void removerconta (String conta) throws SQLException {
+		String SQLInsert = "delete from cliente where numconta = '" + conta + "'";
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate(SQLInsert);
+	}
+	
+	public boolean verificaconta (String conta) throws SQLException{
+		Statement stmt = con.createStatement();
+		int cont = 0;
+		
+		ResultSet dados = stmt.executeQuery("select numconta from cliente");
+		if (dados.isBeforeFirst()) {
+			
+			while (dados.next()) {
+				String teste = dados.getString(1);
+				if (conta.equals(teste)) {
+					cont++;
+				}
+			}
+			if (cont > 0) return true;
+			else return false;
+		}
+		return false;
+	}
+	
+	public void criarconta (String nome, String uf, String agencia, String numconta, int idade, String cpf, String senha, String tipodeconta, double renda, double saldo, double divida, String telefone, double valordoemprestimo) throws SQLException {
+		
+		//System.out.println("insert into cliente (nome,uf,agencia,numconta,idade,cpf,senha,tipodeconta,renda,saldo,divida,telefone) VALUES ('" + nome + "'," + "'" + uf + "'," + "'" + agencia + "','" + numconta + "'," + idade + ",'" + cpf + "','" + senha + "','" + tipodeconta + "'," + renda + "," + saldo + "," + divida +  ",'" + telefone + "')");
+		String SQLInsert = "insert into cliente (nome,uf,agencia,numconta,idade,cpf,senha,tipodeconta,renda,saldo,divida,telefone,valordoemprestimo) VALUES ('" + nome + "'," + "'" + uf + "'," + "'" + agencia + "','" + numconta + "'," + idade + ",'" + cpf + "','" + senha + "','" + tipodeconta + "'," + renda + "," + saldo + "," + divida +  ",'" + telefone + "'," + valordoemprestimo +")";
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate(SQLInsert);
+		
+	}
+	
+	public void autorizaemprestimo (String conta, double valorrequerido, double propostafinal) throws SQLException {
+		// valorp = 0
+		// saldo += valorrequerido
+		// divida += propostafinal
+		// emprestimo = false
+		
+		double saldoantigo = 0, dividaantiga = 0;
+		
+		Statement stmt = con.createStatement();
+		
+		ResultSet dados = stmt.executeQuery("select numconta,valordoemprestimo,valorrequerido, saldo, divida from cliente");
+		if (dados.isBeforeFirst()) {
+			
+			while (dados.next()) {
+				String teste = dados.getString(1);
+				if (conta.equals(teste)) {
+					saldoantigo = dados.getDouble(4);
+					dividaantiga = dados.getDouble(5);
+				}
+			}
+		}
+		double saldoatual = saldoantigo + valorrequerido;
+		double dividatual = dividaantiga + propostafinal;
+		
+		String SQLInsert = "update cliente set emprestimo = " + false + ", valordoemprestimo = " + 0 + ", valorrequerido = " + 0 + ", saldo = " + saldoatual + ", divida = " + dividatual + " where numconta = '" + conta + "'";
+		Statement stmts = con.createStatement();
+		stmts.executeUpdate(SQLInsert);
+	}
+	
+	void excluirconta () {
+		
+	}
+	
+	public ArrayList<String []> avaliaremprestimo (String agencia) throws SQLException {
+		Statement stmt = con.createStatement();
+		String numconta, tipodeconta, renda, saldo, divida, valordoemprestimo, valorrequerido, parcelas, propostafinal;
+		boolean emprestimo;
+		
+		ResultSet dados = stmt.executeQuery("select agencia, numconta, tipodeconta, renda, saldo, divida, emprestimo, valordoemprestimo, valorrequerido, parcelas, propostafinal from cliente");
+		if (dados.isBeforeFirst()) {
+			
+			while (dados.next()) {
+				String teste = dados.getString(1); //agencia
+				if (agencia.equals(teste)) {
+					emprestimo = dados.getBoolean(7);
+					if (emprestimo) {
+						numconta = dados.getString(2);
+						valordoemprestimo = dados.getString(8);
+						valorrequerido = dados.getString(9);
+						tipodeconta = dados.getString(3);
+						renda = dados.getString(4);
+						saldo = dados.getString(5);
+						divida = dados.getString(6);
+						parcelas = dados.getString(10);
+						propostafinal = dados.getString(11);
+						//System.out.println (numconta + " " + tipodeconta + " "+ renda + " " +saldo + " "  +divida +" " + valordoemprestimo +" " + valorrequerido);
+						String[] s = {numconta, tipodeconta, renda, saldo, divida, valordoemprestimo, valorrequerido, parcelas, propostafinal};
+						pedidosdeemprestimo.add(s);
+					}
+				}
+			}
+		}
+		
+		return pedidosdeemprestimo;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+	
+	public static String formatarCPF(String cpf) {
+	    cpf = cpf.replaceAll("[^0-9]", "");
+	        // Aplica a formatação do CPF (###.###.###-##)
+	    return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9);
+   }
+	
+	public static String formatarTelefone(String telefone) {
+	    // Remove qualquer caracter que não seja um dígito
+	    telefone = telefone.replaceAll("[^0-9]", "");
+
+	    // Aplica a formatação do telefone (xx) xxxx-xxxx
+	    return "(" + telefone.substring(0, 2) + ") " + telefone.substring(2, 7) + "-" + telefone.substring(7, 11);
+	}
+
+}
